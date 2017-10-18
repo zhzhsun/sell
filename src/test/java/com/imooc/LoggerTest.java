5457ff9cee2ef32b1bd9e2390cda93b5f6441d6d
sunzhenzhou <sunzhenzhou@wecash.net>
2017-10-18 17:28:59 +0800

    框架搭建

---
+++ src/test/java/com/imooc/LoggerTest.java
@@ -0,0 +1,26 @@
+package com.imooc;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by sunzhenzhou on 2017/10/18.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class LoggerTest extends SellApplicationTests {
+
+    @Test
+    public void test1(){
+        String name = "imooc";
+        String password = "123456";
+        log.debug("debug...");
+        log.info("name:" + name + ",password:" + password);
+        log.info("name:{},password:{}",name,password);
+        log.error("error...");
+    }
+}
